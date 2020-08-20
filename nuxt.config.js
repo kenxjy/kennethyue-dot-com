260c05c1fd97c0ffe26b60c8e12af02b464dfc07
kenxjy <kenneth@kennethyue.com>
2020-08-20 19:22:04 -0400

    added more SEO stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,9 @@
+const name = 'Kenneth Yue';
+const description =
+  'A Toronto-based Software Developer with a passion for technology';
+const url = 'https://kennethyue.com/';
+const image = url + 'images/header-background.jpeg';
+
 export default {
   /*
    ** Nuxt rendering mode
@@ -18,30 +24,49 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'author',
-        name: 'author',
-        content: 'Kenneth Yue',
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'Kenneth Yue, Full-Stack Developer',
-      },
-      {
-        hid: 'keywords',
-        name: 'keywords',
-        content: 'Kenneth Yue, Full-Stack Developer',
-      },
+      { name: 'title', content: name },
+      { name: 'description', content: description },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: url },
+      { property: 'og:title', content: name },
+      { property: 'og:description', content: description },
+      { property: 'og:image', content: image },
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:url', content: url },
+      { property: 'twitter:title', content: name },
+      { property: 'twitter:description', content: description },
+      { property: 'twitter:image', content: image },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/favicon-16x16.png'},
-      { rel: 'icon', type: 'image/png', sizes: "32x32", href: '/favicon-32x32.png'},
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png'},
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-icon-180x180.png',
+      },
       { ref: 'manifest', href: '/manifest.json' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap',
+      },
     ],
     script: [
       {
@@ -58,7 +83,11 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['@/plugins/animateOnScroll.js', '@/plugins/data.js', '@/plugins/smoothScroll.js'],
+  plugins: [
+    '@/plugins/animateOnScroll.js',
+    '@/plugins/data.js',
+    '@/plugins/smoothScroll.js',
+  ],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
