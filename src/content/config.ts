f81c8d39f03c575af79ab3108dd45eba108f23e0
Kenneth Yue <kennethyue39@gmail.com>
2023-04-19 14:06:27 -0400

    reordered fields and removed updateDate

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,18 +4,17 @@ export const collections = {
   work: defineCollection({
     schema: z.object({
       title: z.string(),
+      pubDate: z.coerce.date(),
       lastModified: z.coerce.date().optional(),
       editorsNote: z.string().optional(),
       role: z.string().optional(),
-      timeText: z.string().optional(),
-      description: z.string(),
       startDate: z.coerce.date(),
       endDate: z.nullable(z.coerce.date()),
-      pubDate: z.coerce.date(),
-      updateDate: z.coerce.date().optional(),
-      tags: z.array(z.string()),
+      timeText: z.string().optional(),
       img: z.string(),
-      img_alt: z.string().optional()
-    })
-  })
+      img_alt: z.string().optional(),
+      description: z.string(),
+      tags: z.array(z.string()),
+    }),
+  }),
 } as const;
