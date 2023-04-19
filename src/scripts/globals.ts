17e6f99b3dc772930eeab02b8e47647a50647808
Kenneth Yue <kennethyue39@gmail.com>
2023-04-19 14:06:01 -0400

    initial commit for new website

---
+++ src/scripts/globals.ts
@@ -0,0 +1,4 @@
+export const NAME = 'Kenneth Yue';
+export const EMAIL = 'kenneth@kennethyue.com';
+export const BASE_URL = 'https://kennethyue.com/';
+export const OG_IMAGE = BASE_URL + 'images/og-image.jpg';
