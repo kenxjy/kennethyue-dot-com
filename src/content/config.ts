96a05e8e27646a379c0ed54b7e1dbf14f33a81fc
Kenneth Yue <kennethyue39@gmail.com>
2023-04-19 14:06:27 -0400

    Revert "added new fields and reordered them"
    This reverts commit 739b771734a1b66ea0c3d1a7ab45c39e79519ac4.

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,17 +4,16 @@ export const collections = {
   work: defineCollection({
     schema: z.object({
       title: z.string(),
-      pubDate: z.coerce.date(),
-      lastModified: z.coerce.date().optional(),
-      editorsNote: z.string().optional(),
       role: z.string().optional(),
-      startDate: z.coerce.date(),
-      endDate: z.nullable(z.coerce.date()),
       timeText: z.string().optional(),
       description: z.string(),
-      img: z.string(),
-      img_alt: z.string().optional(),
+      startDate: z.coerce.date(),
+      endDate: z.nullable(z.coerce.date()),
+      pubDate: z.coerce.date(),
+      updateDate: z.coerce.date().optional(),
       tags: z.array(z.string()),
-    }),
-  }),
+      img: z.string(),
+      img_alt: z.string().optional()
+    })
+  })
 } as const;
