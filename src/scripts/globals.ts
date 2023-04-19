f45c85cc88473639b1014e3e4efa137751918996
Kenneth Yue <kennethyue39@gmail.com>
2023-04-19 14:06:01 -0400

    added links object to globals

--- src/scripts/globals.ts
+++ src/scripts/globals.ts
@@ -2,3 +2,8 @@ export const NAME = 'Kenneth Yue';
 export const EMAIL = 'kenneth@kennethyue.com';
 export const BASE_URL = 'https://kennethyue.com/';
 export const OG_IMAGE = BASE_URL + 'images/og-image.jpg';
+export const LINKS = {
+  email: 'mailto:' + EMAIL,
+  linkedin: 'https://www.linkedin.com/in/kenxjy',
+  github: 'https://github.com/kenxjy'
+} as const;
