36799ab924855ea51bed306140e28ec248199c72
Kenneth Yue <kennethyue39@gmail.com>
2023-05-17 14:25:36 -0400

    added some tailwind types

---
+++ src/scripts/tailwindTypes.ts
@@ -0,0 +1,5 @@
+export type FontSize = 'sm' | 'base' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl';
+export type FontSizeClass = `text-${FontSize}`;
+
+export type ItemsPosition = 'start' | 'center' | 'end';
+export type ItemsPositionClass = `items-${ItemsPosition}`;
