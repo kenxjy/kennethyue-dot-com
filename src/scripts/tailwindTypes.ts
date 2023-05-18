0f8c6b48983b5fb159e8fadf3533bbd77b796187
Kenneth Yue <kennethyue39@gmail.com>
2023-05-18 10:46:11 -0400

    Update: Add additional utility classes for tailwind

--- src/scripts/tailwindTypes.ts
+++ src/scripts/tailwindTypes.ts
@@ -1,5 +1,7 @@
-export type FontSize = 'sm' | 'base' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl';
+type FontSize = 'sm' | 'base' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl';
 export type FontSizeClass = `text-${FontSize}`;
 
-export type ItemsPosition = 'start' | 'center' | 'end';
+type ItemsPosition = 'start' | 'center' | 'end';
 export type ItemsPositionClass = `items-${ItemsPosition}`;
+
+export type Screens = 'md' | 'lg';
